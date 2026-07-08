d7bcb0d14611ca6e0af5e7283c5abe0fcac6d9b8
Jan Simon <janesss@gmx.de>
2026-07-08 13:39:27 +0200

    add automatic profile refresh (v0.5)

--- homarr-steam-widget/public/js/app.js
+++ homarr-steam-widget/public/js/app.js
@@ -1,14 +1,21 @@
 import { getSteamProfile } from "./api.js";
 import { renderProfile } from "./ui.js";
+import { REFRESH_INTERVAL } from "./config.js";
 
-async function init() {
+async function loadProfile() {
   try {
     const profile = await getSteamProfile();
 
     renderProfile(profile);
-  } catch (err) {
-    console.error(err);
+  } catch (error) {
+    console.error(error);
   }
+}
+
+async function init() {
+  await loadProfile();
+
+  setInterval(loadProfile, REFRESH_INTERVAL);
 }
 
 init();
